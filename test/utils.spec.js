25e9d64941d67ca5b8bf535c7a874f720f4ff688
sadpig <sadpig@vip.qq.com>
2016-07-22 16:51:40 +0800

    test

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,8 +1,51 @@
+/* eslint-disable */
 import { expect } from 'chai';
-import * as Utils  from '../src/Utils.js';
+import * as Utils  from '../lib/Utils.js';
 
 
 describe('UtilsTest', () => {
+	describe('formatMonthTitle生成对应的月份文案', () => {
+		it('文案对应', () => {
+			const date = new Date(2016, 5, 2);
+			const ret = Utils.formatMonthTitle(date);
+			expect(ret).to.equal('2016年 六月');
+		});
+		it('一个月的两天生成的title应该一致', () => {
+			const date = new Date(2016, 5, 2);
+			const date2 = new Date(2016, 5, 1);
+			const ret = Utils.formatMonthTitle(date);
+			expect(ret).to.equal(Utils.formatMonthTitle(date2));
+		});
+	});
+
+	describe('formatWeekday生成日期标题', () => {
+		it('生成日-六的标题文案', () => {
+			let ret = [];
+			for (let i = 0; i < 7; i++) {
+				ret.push(Utils.formatWeekday(i));
+			}
+			expect(ret.join('')).to.equal('日一二三四五六');	
+		})
+	});
+	describe('getMonths', () => {
+		it('生成1-12月文案', () => {
+			let ret = [];
+			let months = Utils.getMonths();
+			for (let i = 0; i < months.length; i++) {
+				ret.push(months[i]);
+			}
+			expect(ret.join('')).to.equal('一月二月三月四月五月六月七月八月九月十月十一月十二月');	
+		})
+	});
+
+	describe('clone复制一个Date对象', () => {
+		it('clone复制一个Date对象', () => {
+			let date = new Date();
+			let ret = Utils.clone(date);
+			expect(ret.toGMTString()).to.equal(date.toGMTString());
+		});
+	});
+
 	describe('addMonths', () => {
 		it('日期+2个月', () => {
 			const date = new Date(2016, 1);
@@ -101,39 +144,111 @@ describe('UtilsTest', () => {
 		});
 
 	});
+	describe('getFirstDayOfMonth获取某个月第一天', () => {
+		it('获取某个月第一天', () => {
+			const month = new Date();
+			const ret = Utils.getFirstDayOfMonth(month);
+			expect(ret.getDate()).to.equal(1);
+		});
+	});
 
-	// describe('addDayToRange设置一个日期范围', () => {
-	// 	it('设置起始的日期', () => {
-	// 		const day = new Date();
-	// 		const range = Utils.addDayToRange(day);
-	// 		expect(range).to.deep.equal({ from: day, to: null});
-	// 	});
+	describe('getDayNumOfMonth获取某个月的总天数', () => {
+		it('获取一个月的天数', () => {
+			const day = new Date();
+			const ret = Utils.getDayNumOfMonth(day);
+			day.setDate(parseInt(ret) + 1)
+			expect(day.getDate()).to.equal(1);
+		});
+	});
 
-	// 	it('在已经有起始点后设置范围终点', () => {
-	// 		const from = new Date(2016,0,1);
-	// 		const to = new Date();
-	// 		const range = Utils.addDayToRange(to, { from: from })
-	// 		expect(range).to.deep.equal({ from, to: to });
-	// 	});
-	// 	it('再次设置同一天的话就初始化', () => {
-	// 		const day = new Date();
-	// 		let range = { from: day, to: day};
-	// 		range = Utils.addDayToRange(day, range);
-	// 		expect(range).to.deep.equal({
-	// 			from: null,
-	// 			to: null
-	// 		});
-	// 	});
-	// 	it('如果已经有一个范围，选择了一天比起始点早，则重置起始点', () => {
-	// 		const day = new Date(2016, 6, 1);
-	// 		const to = new Date(2016, 6, 20);
-	// 		let range = { from: new Date(2016, 6, 5), to };
-	// 		range = Utils.addDayToRange(day, range);
-	// 		expect(range).to.deep.equal({ 
-	// 			from: day, 
-	// 			to: to 
-	// 		});
-	// 	});
+	describe('getWeekArray按星期分割每个月的天数', () => {
+		it('按星期分割本月', () => {
+			const day = new Date();
+			const ret = Utils.getWeekArray(day);
+			let index = -1;
+			ret.forEach((v, k) => {
+				v.forEach((v2, k2) => {
+					if (v2.getDate() == day.getDate()) {
+						index = k2;
+					}
+				});
+			})
+			expect(index).to.equal(day.getDay());
+		});
+	});
 
-	// });
+	describe('getModifiersForDay自定义修饰器判断', () => {
+		it('自定义修饰器，返回selected修饰器', () => {
+			let day = new Date(2016, 6, 20);
+			let modifiers = {
+				selected: (day) => {
+					let day1 = new Date(2016, 6, 1);
+					let day2 = new Date(2016, 6, 30);
+					if (day > day1 && day < day2 ) {
+						return true
+					}else{
+						return false
+					}
+				}
+			}
+			let ret = Utils.getModifiersForDay(day, modifiers);
+			let name = ret.join('');
+			let name_source = Object.keys(modifiers);
+			expect(name).to.equal(name_source.join(''));
+		});
+		it('自定义修饰器，返回disabled修饰器', () => {
+			let day = new Date(2016, 6, 20);
+			let modifiers = {
+				selected: (day) => {
+					let day1 = new Date(2016, 6, 1);
+					let day2 = new Date(2016, 6, 30);
+					if (day < day1 && day < day2 ) {
+						return true
+					}else{
+						return false
+					}
+				},
+				disabled: (day) => {
+					let day1 = new Date(2016, 6, 1);
+					let day2 = new Date(2016, 6, 30);
+					if (day > day1 && day < day2 ) {
+						return true
+					}else{
+						return false
+					}
+				}
+			}
+			let ret = Utils.getModifiersForDay(day, modifiers);
+			let name = ret.join('');
+			expect(name).to.equal('disabled');
+		});
+		it('自定义修饰器，返回所有的修饰器', () => {
+			let day = new Date(2016, 6, 20);
+			let modifiers = {
+				selected: (day) => {
+					let day1 = new Date(2016, 6, 1);
+					let day2 = new Date(2016, 6, 30);
+					if (day > day1 && day < day2 ) {
+						return true
+					}else{
+						return false
+					}
+				},
+				disabled: (day) => {
+					let day1 = new Date(2016, 6, 1);
+					let day2 = new Date(2016, 6, 30);
+					if (day > day1 && day < day2 ) {
+						return true
+					}else{
+						return false
+					}
+				}
+			}
+			let ret = Utils.getModifiersForDay(day, modifiers);
+			let name = ret.join('');
+			let name_source = Object.keys(modifiers).join('');
+			expect(name).to.equal(name_source);
+		});
+	});
 });
+/* eslint-disable */
